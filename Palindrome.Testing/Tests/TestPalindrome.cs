221d3e8e55842c065e1f36e9f38757f1b0d42283
Carlos Ch <cvchavez92@gmail.com>
2020-07-20 15:01:36 -0600

    added more test cases

--- Palindrome.Testing/Tests/TestPalindrome.cs
+++ Palindrome.Testing/Tests/TestPalindrome.cs
@@ -8,10 +8,53 @@ namespace Palindrome.Testing.Test
         [Fact]
         public void TestIsPalindrome()
         {
-          var sut = new Palind();
+          var sut = new Palindrome();
           string s = "carlos";
 
           Assert.False(sut.IsPalindrome(s));
+        }
+
+        /*
+        Following test cases assert string passed is a palindrome
+        */
+        [Theory]
+        [InlineData("waaw")]
+        [InlineData("abba")]
+        [InlineData("deed")]
+        public void TestIsPalindrome2(string s)
+        {
+          var sut = new Palindrome();
+
+          Assert.True(sut.IsPalindrome(s));
+        }
+
+        /*
+        Following test cases assert string passed is NOT a palindrome
+        */
+        [Theory]
+        [InlineData("this is not a palindrome")]
+        [InlineData("hellow guys")]
+        [InlineData("NOT A PALINDROME!")]
+        public void TestIsPalindrome3(string s)
+        {
+          var sut = new Palindrome();
+
+          Assert.False(sut.IsPalindrome(s));
+        }
+
+        /*
+        Following test cases handle special characters
+        All input are palindromes, method assert input is a palindrome (true)
+        */
+        [Theory]
+        [InlineData("w**w")]
+        [InlineData("-bb-")]
+        [InlineData("?aa?")]
+        public void TestIsPalindrome2(string s)
+        {
+          var sut = new Palindrome();
+
+          Assert.True(sut.IsPalindrome(s));
         }
     }
 }
